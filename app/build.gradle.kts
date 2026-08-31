79b69f2eae09b825bc64a519d8faa72a6d4d6cda
dsh <dsh@localhost>
2026-08-31 22:49:10 +0800

    Bump version to 5.4.464.1221

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         
         val overrideVersionCode = project.findProperty("versionCodeOverride")?.toString()?.toIntOrNull()
         val overrideVersionName = project.findProperty("versionNameOverride")?.toString()
-        versionCode = overrideVersionCode ?: 544641220
-        versionName = overrideVersionName ?: "5.4.464.1220 Beta"
+        versionCode = overrideVersionCode ?: 544641221
+        versionName = overrideVersionName ?: "5.4.464.1221 Beta"
 
         val overrideReleaseDate = project.findProperty("releaseDateOverride")?.toString()
         buildConfigField("String", "RELEASE_DATE", "\"${overrideReleaseDate ?: "2026-08-25"}\"")
